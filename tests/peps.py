23502dcbc35b208e0b4269b4699776331fb17ce9
Alhadis <gardnerjohng@gmail.com>
2022-09-04 18:40:44 +1000

    Rewrite rules for matching PEP and RFC references

---
+++ tests/peps.py
@@ -0,0 +1,31 @@
+PEPs and pips
+=============
+
+Before :pep:`Label<440#version-specifiers>` After
+
+:pep:`440`
+
+:pep:`440#version-specifiers`
+
+:pep:`440  # version-specifiers`
+
+:pep:`440   #
+version-specifiers`
+
+:pep:`440#version-specifiers`
+
+:pep:`440#version-\`backticks\``
+
+:pep:`440 is the number of this section <440#version-\<\`angle\\\` bracket\><-specifiers>`
+
+:pep:`300#<301#this-isn't-sparta>`
+
+:pep:`440#version-<>`
+
+:pep:`440#version-<440#\`angle\` bracket-specifiers>`
+
+:pep:`420
+weed lol
+<420#blaze-it-fgt>`
+
+`Label <link>`_
